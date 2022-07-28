ba38196fecff041d06930cecb3b438b26ae1c312
gag <256sha9gag@gmail.com>
2022-07-28 21:16:38 +0300

    refact: bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,43 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import startBrainEven from '../games/module-brain-even.js';
 
-let randomInt = 0;
-let userName = '';
-let userAnswer = '';
-let trueAnswer = '';
-
-const getRandomInt = (max) => Math.floor(Math.random() * max);
-
-const getIsEven = (num) => {
-    if (num % 2 === 0) {
-        return 'yes';
-    }
-    return 'no';
-};
-
-const getBrainEvenResult = (acc) => {
-    randomInt = getRandomInt(100);
-    trueAnswer = getIsEven(randomInt);
-    console.log(`Question: ${randomInt}`);
-    userAnswer = readlineSync.question('Your answer: ');
-
-    if (trueAnswer !== userAnswer) {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`);
-        return console.log(`Let's try again, ${userName}!`);
-    }
-
-    if (acc < 2) {
-        console.log('Correct!');
-        return getBrainEvenResult(acc + 1);
-    }
-
-    console.log('Correct!');
-    return console.log(`Congratulations, ${userName}!`);
-};
-
-console.log('Welcome to the Brain Games!');
-userName = readlineSync.question('May I have your name?: ');
-console.log(`Hello, ${userName}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-getBrainEvenResult(0);
+startBrainEven();
