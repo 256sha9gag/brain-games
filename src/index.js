1db987b76e367e31cc9429345ee9cd094ab32de4
gag <256sha9gag@gmail.com>
2022-07-29 16:38:29 +0300

    refact: Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import readlineSync from 'readline-sync';
 
-let userName = '';
-
-const greeting = () => {
+const getBrainGames = (getParamGame, terms) => {
     console.log('Welcome to the Brain Games!');
-    userName = readlineSync.question('May I have your name?: ');
-    return console.log(`Hello, ${userName}!`);
+    const userName = readlineSync.question('May I have your name?: ');
+    console.log(`Hello, ${userName}!`);
+    console.log(terms);
+
+    const getStepGame = (acc) => {
+        const [question, trueAnswer] = getParamGame();
+        console.log(`Question: ${question}`);
+        const userAnswer = readlineSync.question('Your answer: ');
+
+        if (String(trueAnswer) !== String(userAnswer)) {
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`);
+            return console.log(`Let's try again, ${userName}!`);
+        }
+
+        if (acc < 2) {
+            console.log('Correct!');
+            return getStepGame(acc + 1);
+        }
+
+        console.log('Correct!');
+        return console.log(`Congratulations, ${userName}!`);
+    };
+    return getStepGame(0);
 };
 
 const getRandom = (max) => Math.floor(Math.random() * max);
 
-export { greeting, getRandom, userName };
+export { getRandom, getBrainGames };
